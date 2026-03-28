2a666ec2306c42223461f7fcae6c5fd4dae2871c
yashkumararya1 <yash.kumararya@medecro.ai>
2026-03-28 19:24:47 +0530

    build: deploy to pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig(({ command }) => ({
-  base: command === 'serve' ? '/' : '/SystemPrinting/',
+  base: command === 'serve' ? '/' : '/SystemPrinting-live/',
   server: {
     open: true,
   },
   build: {
-    outDir: 'dist',
+    outDir: 'docs',
+    emptyOutDir: true,
     assetsInlineLimit: 0,
   },
 }));
